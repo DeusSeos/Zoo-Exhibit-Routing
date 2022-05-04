4c0528a9edaca5681c851cc71187dc07ac811f3f
alian <jakepaulson37@gmail.com>
2022-05-04 13:33:45 -0700

    added example graph generatio

--- app/src/main/java/com/example/zooseeker/DirectionActivity.java
+++ app/src/main/java/com/example/zooseeker/DirectionActivity.java
@@ -7,12 +7,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
+import org.jgrapht.Graph;
+import org.jgrapht.GraphPath;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+
 import java.util.LinkedHashSet;
+import java.util.Map;
 
 public class DirectionActivity extends AppCompatActivity {
 
     private ListView directionList;
     private Button nextButton;
+    String zooJsonName;
     LinkedHashSet<SearchListItem> selectedItems = (LinkedHashSet<SearchListItem>) getIntent().getSerializableExtra("selected_list");
 
     @Override
@@ -23,11 +29,15 @@ public class DirectionActivity extends AppCompatActivity {
         directionList = findViewById(R.id.direction_list);
         nextButton = findViewById(R.id.next_button);
 
-        //generate the graph
-
+        //generate the graph - Currently uses example asset
+        Graph<String, IdentifiedWeightedEdge> g = ZooData.loadZooGraphJSON(this, "sample_zoo_graph.json");
+        Map<String, ZooData.VertexInfo> vInfo = ZooData.loadVertexInfoJSON(this, "sample_node_info.json");
+        Map<String, ZooData.EdgeInfo> eInfo = ZooData.loadEdgeInfoJSON(this, "sample_edge_info.json");
 
         //create a new list view
         //parse the locations that are selected and run dijkstra between each node
+        GraphPath<String, IdentifiedWeightedEdge> path = DijkstraShortestPath.findPathBetween(g, start, goal);
+
         //save the directions
 
         //display the directions in our directionList
