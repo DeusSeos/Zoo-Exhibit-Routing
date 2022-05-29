f8385ba23cce806d7b9fd021b0294290f6cfa3bf
Lainey <jil172@ucsd.edu>
2022-05-28 19:26:18 -0700

    --noedit

--- app/src/main/java/com/example/zooseeker/DirectionActivity.java
+++ app/src/main/java/com/example/zooseeker/DirectionActivity.java
@@ -71,7 +71,7 @@ public class DirectionActivity extends AppCompatActivity {
             directionList.setAdapter(directionsAdapter);
         });
 
-        skipButton.setOnClickListener()
+
     }
 
 }
