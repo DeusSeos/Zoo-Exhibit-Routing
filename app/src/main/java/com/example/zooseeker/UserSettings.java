5567945f9a8b3315bf66bebb0ba5bcb52c112111
DeusSeos <45889782+DeusSeos@users.noreply.github.com>
2022-05-31 15:23:21 -0700

    changes

--- app/src/main/java/com/example/zooseeker/UserSettings.java
+++ app/src/main/java/com/example/zooseeker/UserSettings.java
@@ -7,8 +7,6 @@ public class UserSettings extends Application {
     public static final String PREFERENCES = "Preferences";
 
     public static final String CUSTOM_DIRECTION = "false";
-    public static final String BRIEF_DIRECTION = "briefDirection";
-    public static final String DETAILED_DIRECTION = "detailedDirection";
 
     private boolean customDirection;
 
